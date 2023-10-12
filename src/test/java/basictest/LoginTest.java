b4a4c551f00ae253f5b1390386c89827d3cb1589
Lutyvr02 <lutyvr02@gmail.com>
2023-10-12 13:36:29 -0400

    tarea commit

--- src/test/java/basictest/LoginTest.java
+++ src/test/java/basictest/LoginTest.java
@@ -1,7 +1,6 @@
 package basictest;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
